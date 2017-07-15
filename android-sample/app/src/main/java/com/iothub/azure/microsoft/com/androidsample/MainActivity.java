6b739a00237d6e4f76e2d30f06ebf05aa422def9
SoarHuang <soarhuang.aopen@gmail.com>
2017-07-16 01:57:35 +0800

    import Azure Iot Hub SDK

--- android-sample/app/src/main/java/com/iothub/azure/microsoft/com/androidsample/MainActivity.java
+++ android-sample/app/src/main/java/com/iothub/azure/microsoft/com/androidsample/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.microsoft.azure.sdk.iot.device.*;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
-    String connString = "[device connection string]";
+    String connString = "Azure Iot Hub Connect link";
     String deviceId = "MyAndroidDevice";
     double temperature;
     double humidity;
